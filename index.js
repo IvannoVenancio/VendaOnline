7b354eb267436aa6a31507c5ecbe28205b6e79d5
Wendy Costa M <costawendy91@gmail.com>
2024-11-05 08:35:38 +0100

    Tabelas no prisma

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const express = require ("express");
 
 const app = express();
 
+const handlebars = require ('express-handlebars')
+
+//Config
+    //Template Engine
+    app.engine('handlebars',handlebars({defaultLayout:'main'}))
+    app.set('view engine', 'handlebars')
+
+
 //Rota1
 
 app.get("/", function(req, res){
-    res.send("")
+    res.render("home")
 
 });
 
