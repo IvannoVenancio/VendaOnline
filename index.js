5ebe06cccc105e3ffa1dfde9969c16fd02bc9850
Wendy Costa M. <costawendy91@gmail.com>
2024-11-07 18:53:35 +0100

    changes

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get("/", function(req, res){
 
 
 
-
-app.listen(8081, function(){
+const PORT = 8081
+app.listen(8081, function() {
     console.log("servidor rodando na url http://localhost:8081");
 });
