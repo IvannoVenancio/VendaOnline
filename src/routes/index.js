f7725afed7c5bfc0c802f38912b37208fbd0e90a
Wendy Costa M. <costawendy91@gmail.com>
2024-11-19 18:08:26 +0100

    atualizações

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const indexRoutes = express.Router()
 const UserController = require('../controllers/UserController')
+const LoginController = require('../controllers/LoginController')
 
 indexRoutes.get('/', UserController.home)
 
 indexRoutes.get('/carrinho', UserController.carrinho)
 
-indexRoutes.get('/login', UserController.login)
+indexRoutes.get('/login', LoginController.login)
 
 indexRoutes.get('/signup', UserController.signup)
 
